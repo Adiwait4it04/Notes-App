82c13d026d37a63338e893abb555402bc713c7a6
Aditya Saha <adityasaha04@gmail.com>
2023-02-25 17:27:52 +0530

    2ndnew

--- lib/services/authservices.dart
+++ lib/services/authservices.dart
@@ -1,1 +1,75 @@
+// ignore_for_file: non_constant_identifier_name, body_might_complete_normally_nullable, non_constant_identifier_names
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class Authservice {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  Future<User?> Register(
+      String email, String password, BuildContext context) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.message.toString(),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<User?> login(
+      String email, String password, BuildContext context) async {
+    try {
+      UserCredential userCredential = await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      return userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.message.toString(),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<User?> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        UserCredential userCredential =
+            await firebaseAuth.signInWithCredential(credential);
+        return userCredential.user;
+      }
+    } catch (e) {
+      print(
+        e.toString(),
+      );
+    }
+  }
+
+  Future signOut() async {
+    await GoogleSignIn().signOut();
+    await firebaseAuth.signOut();
+  }
+}
